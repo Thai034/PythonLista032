3ea7ef63dac507977f5be4196618c7270df33c7f
Thaila Alves <thailasalvees@gmail.com>
2023-08-08 11:42:35 -0300

    todos os exercicios resolvidos 08/08/23

--- exercicios032/questao09.py
+++ exercicios032/questao09.py
@@ -5,6 +5,9 @@ em dias. Obs: Considere os anos com 365 dias e os meses com 30 dias.
 
 print("Quer saber quantos anos em dias você tem? Ta bom, responda as perguntas abaixo:")
 
+anos = int(input("Quantos anos você tem? "))
+meses = int(input("Quantos meses ja passou desde o seu último aniversário? "))
+dias = int(input("Quantos dias ja faz do seu último aniversário? "))
 
-
-
+resultado = (anos * 365) + (meses * 30) + dias
+print("Você tem", resultado, "dias de vida")
--- exercicios032/questao10.py
+++ exercicios032/questao10.py
@@ -2,3 +2,16 @@
 Escreva um algoritmo pergunte o número total de eleitores de um município, o número de votos brancos, nulos
 e válidos e apresente como resposta o percentual que cada um representa em relação ao total de eleitores.
 '''
+
+print("Quer saber o percentual de seus eleitores? Responda as perguntas abaixo e responderei para você!")
+
+eleitores = int(input("Informe o numero de eleitores do municipio: "))
+brancos = int(input("Informe o número total de votos brancos: "))
+nulos = int(input("Informe o numero total de votos nulos: "))
+validos = int(input("informe o numero total de votos válidos: "))
+
+
+print(f"Votos totais: {eleitores}%")
+print(f"Votos Brancos: {100 * brancos/eleitores:.2f}%")
+print(f"Votos nulos: {100 * nulos/eleitores:.2f}%")
+print(f"Votos válidos: {100 * validos/eleitores:.2f}%")
